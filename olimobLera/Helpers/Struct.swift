1c02f7d7835227830e149d411f1dcaa9e2d7281e
leramovie <leramovie@gmail.com>
2020-01-23 15:16:54 +0300

    some fix

---
+++ olimobLera/Helpers/Struct.swift
@@ -0,0 +1,17 @@
+//
+//  Struct.swift
+//  olimobLera
+//
+//  Created by Valery Shel on 23.01.2020.
+//  Copyright © 2020 Parakluence. All rights reserved.
+//
+
+import UIKit
+
+
+struct fbdata {
+    var promo_code: String?
+    init(promo_code: String){
+        self.promo_code = promo_code
+    }
+}
